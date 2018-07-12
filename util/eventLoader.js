40a2bc9bbd4eaf6be792ebabfe46be1a7a9dc691
Proxxaaa <40413110+Proxxaaa@users.noreply.github.com>
2018-07-12 18:15:49 -0400

    Removing disconnect.js, since it has no code

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,6 +1,5 @@
 const reqEvent = (event) => require(`../events/${event}`)
 module.exports = client => {
 	client.on('reconnecting', () => reqEvent('reconnecting')(client));
-	client.on('disconnect', () => reqEvent('disconnect')(client));
 	client.on('message', reqEvent('message'));
 };
