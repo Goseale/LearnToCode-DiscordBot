65f6a4d7dd0979af61b6aedd4db7e0222709efa0
Proxxaaa <40413110+Proxxaaa@users.noreply.github.com>
2018-07-13 18:27:17 -0400

    Finishing up Arrays

--- commands/js.js
+++ commands/js.js
@@ -31,7 +31,24 @@ Maybe even single quotes (') if you don't care to escape them with backslashes."
             color: 777777,
             title: "LearnToCode BOT - JS Arrays",
             description: "Arrays are like lists. You know, like that shopping list that you totally put off last \
-week. Yes, I know about that. Arrays should always be assigned to a variable"
+week. Yes, I know about that. Arrays should always be assigned to a variable. It is suggested you use a `let` variable (More on those in another \
+course.) as such: `let array = ` and then the array. Arrays look like this: `['foo', 'bar', 'item3']`, and to add more items, you just \
+put a comma  (`,`) after the last item and type it up. Items can be any one of the **`Primitive Types`** mentioned in the last \
+course. If you call an array, it will simply print out the contents of the array, or everything you typed to set the \
+variable as, including the square brackets (`[]`). To call for a specific item, just count the items to the item you \
+want to call, and subtract one from it. Replace x with that number: `arrayName[x]`. You subtract one because JS is \
+0-Indexed, meaning it starts couting at 0.",
+            fields: [
+                {
+                    name: "Try it!",
+                    value: "**1.** In your command prompt, type `node` and hit enter. Then type `let array = ['foo',\
+'bar'] and enter. Now log `array` to the console, no quotes. Hit enter, and voila, you logged the entire array. \
+now log `array[2]`, this should result in an error, because this is calling for the third item, when there is no \
+third item, since JS is zero-indexed.\n **2.** Create your own array called `foobar` with whatever items you want. \
+Go ahead, I'll still be here. Now do `console.log(foobar.shift())`. It should've returned the first item. \
+Now do `console.log(foobar[0])`. It should return the same thing. Right? Wrong. More on this in **Array Actions**."
+                },
+            ]
         }})
     } else
         message.channel.send({
