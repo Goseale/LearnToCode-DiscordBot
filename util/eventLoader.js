56e3812df990f53ea3b2cbc506fa8f16af8c6394
Proxxaaa <40413110+Proxxaaa@users.noreply.github.com>
2018-07-24 11:28:12 -0400

    Didn't need that!

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,5 +1,5 @@
 const reqEvent = (event) => require(`../events/${event}`)
 module.exports = client => {
 	client.on('reconnecting', () => reqEvent('reconnecting')(client));
-	client.on('message', reqEvent('message')(client));
+	client.on('message', reqEvent('message'));
 };
