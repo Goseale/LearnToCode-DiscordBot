fdfcc49ef94dde790a4aa3adb61d071daed14aa1
Proxxaaa <40413110+Proxxaaa@users.noreply.github.com>
2018-07-24 11:22:12 -0400

    kill edits

--- commands/kill.js
+++ commands/kill.js
@@ -1,7 +1,6 @@
 exports.run = function (client, message, args) {
     if (message.author.id === "181068943723724801") {
-        message.channel.send("Shutting down bot");
-        client.destroy();
+        message.author.send("Shutting down bot");
         process.end()
     };
 };
