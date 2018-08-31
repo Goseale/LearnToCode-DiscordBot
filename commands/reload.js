df424193dfc6fc3562be5a317a8e022d7787e8ee
Proxxa <40413110+Proxxaaa@users.noreply.github.com>
2018-08-31 19:42:02 -0400

    delete reload command trigger

--- commands/reload.js
+++ commands/reload.js
@@ -1,5 +1,6 @@
 const main = require('../bot.js');
 exports.run = function(client, message, args) {
+    message.delete(100).catch(e => {})
     let cmd = args.join(' ')
     main.reload(message, cmd)
 };
