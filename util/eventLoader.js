47b8087ec030241064fa4e4510279c0938a6997e
Proxxa <40413110+Proxxaaa@users.noreply.github.com>
2018-09-01 16:56:49 -0400

    adding error to eventLoader

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -3,4 +3,5 @@ console.log(`Start loading events`)
 module.exports = client => {
 	client.on('reconnecting', () => reqEvent('reconnecting')(client));
 	client.on('message', reqEvent('message'));
+	client.on('error', reqEvent('error'))
 };
