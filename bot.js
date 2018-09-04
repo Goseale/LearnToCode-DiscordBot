e02df9dfdddfa0d0952b08f69f7dfe138cc5eeaa
Proxxa <40413110+Proxxaaa@users.noreply.github.com>
2018-09-03 22:16:32 -0400

    -

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ app.get("/", (request, response) => {
 app.listen(process.env.port);
 setInterval(() => {
 	http.get(`${process.env.host}`)
-}, 240000)
+}, 1790000)
 
 require('./util/eventLoader')(client);
 
