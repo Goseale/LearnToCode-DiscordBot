249e3e3e673d5feb3a6ae8282291272b7f59a4ff
Proxxa <40413110+Proxxaaa@users.noreply.github.com>
2018-09-03 21:41:16 -0400

    --

--- bot.js
+++ bot.js
@@ -1,28 +1,13 @@
-// Before we do ANYTHING, we're setting up some basic variables.
-// You should have this code downloaded and have your very own settings.json file that you made.
-// This file is not included since we don't want to break the confidentiality of anyone's copy of the bot.
-// .gitignore has settings.json already ignored. So Proxxaaa's copy of settings.json isn't given away to everyone in the world who sees this.
 const settings = require('./settings.json');
 const prefix = settings.prefix;
-const token = settings.token;
-// Never give out your token, so keep this file safe at all times.
-// The settings file should look like this
-/*
-{
-  "prefix": "?"
-  "token": "TOKEN"
-}
-*/
-
-// Obviously, don't include the /**/, those are to comment out that text. Keep the curly brackets, however.
-// Also, don't forget you need at least Node 6.0.0 and the latest version of Discord.JS to host your very own version of the bot.
-
+const token = process.env.botToken;
 
 const Discord = require('discord.js')
 const client = new Discord.Client()
-client.login(process.env.botToken)
-const app = require('express')
+client.login(token)
+const express = require('express')
 const http = require('http')
+const app = express()
 
 app.get("/", (request, response) => {
 	console.log("Ping Received")
